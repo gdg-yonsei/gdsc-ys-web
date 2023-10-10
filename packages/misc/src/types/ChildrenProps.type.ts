4c6e65c5af37e534f8ac0d817c3e2436b73b8de1
whatisyourname0 <junii@whatssub.co>
2023-10-10 22:07:12 +0900

    docs: add ChildrenProps documentation

--- packages/misc/src/types/ChildrenProps.type.ts
+++ packages/misc/src/types/ChildrenProps.type.ts
@@ -4,5 +4,7 @@ import type { PropsWithChildren } from 'react';
  * ### ChildrenProp
  *
  * Preset for component props typing when children is needed
+ *
+ * @abstract
  */
 export interface ChildrenProp extends PropsWithChildren<{}> {}
